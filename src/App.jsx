1ab1cba91083dfb5ec71960cf1b45406b2d4492e
Linh Duy <duypnl219@gmail.com>
2022-11-03 21:59:19 +0700

    upload NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import { Route, Router, Routes } from "react-router-dom";
+import ProfileLayout from "./layouts/ProfileLayout";
+import MainLayout from "./layouts/MainLayout";
 import Home from "./pages";
+import Profile from "./pages/ca-nhan";
 import Contact from "./pages/hop-tac";
+import Page404 from "./pages/not-found";
 import Team from "./pages/than-vien";
+import Project from "./pages/ca-nhan/du-an";
+import MyCourse from "./pages/ca-nhan/khoa-hoc";
+import Payment from "./pages/ca-nhan/lich-su-thanh-toan";
+import Coin from "./pages/ca-nhan/coin";
+import {
+  PROFILE_PATH,
+  PROFILE_PROJECT_PATH,
+  PROFILE_COURSE_PATH,
+  PROFILE_COIN_PATH,
+  PROFILE_PAYMENT_PATH,
+} from "./config/path";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hop-tac" element={<Contact />} />
-        <Route path="/thanh-vien" element={<Team />} />
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/hop-tac" element={<Contact />} />
+          <Route path="/thanh-vien" element={<Team />} />
+          <Route path={PROFILE_PATH} element={<ProfileLayout />}>
+            <Route index element={<Profile />} />
+            <Route path={PROFILE_PROJECT_PATH} element={<Project />} />
+            <Route path={PROFILE_COURSE_PATH} element={<MyCourse />} />
+            <Route path={PROFILE_COIN_PATH} element={<Coin />} />
+            <Route path={PROFILE_PAYMENT_PATH} element={<Payment />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<Page404 />} />
       </Routes>
-      {/* <Home/> */}
-      <Contact />
-      <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ src/pages/ca-nhan/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Coin from "./coin";
+import Project from "./du-an";
+import MyCourse from "./khoa-hoc";
+import Payment from "./lich-su-thanh-toan";
+
+export default function Profile() {
+  return (
+    <div className="tab1">
+      <label>
+        <p>
+          Họ và tên<span>*</span>
+        </p>
+        <input type="text" placeholder="Nguyễn Văn A" />
+      </label>
+      <label>
+        <p>
+          Số điện thoại<span>*</span>
+        </p>
+        <input type="text" placeholder="0949******" />
+      </label>
+      <label>
+        <p>
+          Email<span>*</span>
+        </p>
+        <input defaultValue="vuong.dang@dna.vn" disabled type="text" />
+      </label>
+      <label>
+        <p>
+          Facebook<span>*</span>
+        </p>
+        <input type="text" placeholder="Facebook url" />
+      </label>
+      <label>
+        <p>
+          Skype<span>*</span>
+        </p>
+        <input type="text" placeholder="Skype url" />
+      </label>
+      <div className="btn main rect">LƯU LẠI</div>
+    </div>
+  );
+}
