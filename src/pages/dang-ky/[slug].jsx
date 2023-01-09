03a6e1c2e125f1928829fdf22e37d56000044b0e
Linh Duy <duypnl219@gmail.com>
2023-01-09 12:52:56 +0700

    update url

--- src/pages/dang-ky/[slug].jsx
+++ src/pages/dang-ky/[slug].jsx
@@ -137,17 +137,6 @@ export default function Register() {
           </div>
         </div>
       </section>
-      {/* <div class="register-success">
-            <div class="contain">
-                <div class="main-title">đăng ký thành công</div>
-                <p>
-                    <strong>Chào mừng Trần Nghĩa đã trở thành thành viên mới của CFD Team.</strong> <br>
-                    Cảm ơn bạn đã đăng ký khóa học tại <strong>CFD</strong>, chúng tôi sẽ chủ động liên lạc với bạn thông qua facebook
-                    hoặc số điện thoại của bạn.
-                </p>
-            </div>
-            <a href="/" class="btn main rect">về trang chủ</a>
-        </div> */}
     </main>
   );
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -2,13 +2,13 @@ import api from "../config/api";
 
 const authService = {
   login(data) {
-    return api.post("/login", data);
+    return api.post("/authentication/v2/login", data);
   },
   register(data) {
     return api.post("/register", data);
   },
   refreshToken(data) {
-    return api.post("/refresh-token", data);
+    return api.post("/authentication/v2/refresh-token", data);
   },
 };
 export default authService;
